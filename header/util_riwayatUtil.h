c0cf3a63439978d92dc4588586feddb9943c66d6
randy-25 <randyrevaldo64@gmail.com>
2024-06-16 18:56:45 +0700

    fix riwayat util header file

--- header/util_riwayatUtil.h
+++ header/util_riwayatUtil.h
@@ -4,10 +4,15 @@
 #include "util_getData.h"
 
 void cariRiwayat(struct riwayat *riwayatPasien, int jumlahRiwayatPasien, char *IdPasien, struct riwayat **dataHolder, int *jumlahDataHolder, int *confirm, char **nameHolder, struct dataPasien *pasien, int jumlahPasien);
+
 int biayaTindakan(char tindakan[100]);
+
 void tambahRiwayat(struct riwayat **riwayatPasien, int *jumlahRiwayatPasien, char *IdPasien, char *tanggal_periksa, char *diagnosis, char *tindakan, char *tanggal_kontrol, struct riwayat *newRiwayatHolder,char **namaHolder, struct dataPasien *pasien, int jumlahPasien, int *confirm);
+
 int cekNomorRiwayat(int nomorRiwayat, char *IdPasien, struct riwayat *riwayatPasien);
+
 void ubahRiwayat(struct riwayat **riwayatPasien, int jumlahRiwayatPasien, int nomorRiwayat, char *tanggal_periksa, char *diagnosis, char *tindakan, char *tanggal_kontrol, struct riwayat *newRiwayatHolder, char **namaHolder, struct dataPasien *pasien, int jumlahPasien);
+
 void hapusRiwayat(struct riwayat **riwayatPasien, int *jumlahRiwayatPasien, int nomorRiwayat);
 
 #endif
