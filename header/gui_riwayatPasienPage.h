d2d68fbd62abda5d739c7fb61bf4ecadcde6a94d
randy-25 <randyrevaldo64@gmail.com>
2024-06-16 18:55:20 +0700

    add header riwayat pasien gui

--- header/gui_riwayatPasienPage.h
+++ header/gui_riwayatPasienPage.h
@@ -21,6 +21,7 @@ void on_cari_riwayatPasien_data_gui(GtkButton *button, gpointer user_data);
 GtkWidget *RiwayatPasienPage_CariData();
 // -------- END OF CARI DATA PASIEN PAGE --------
 
+
 // ------- TAMBAH DATA RIWAYAT PASIEN PAGE -------
 void reset_tambahRiwayat_pasien_page();
 
@@ -31,6 +32,36 @@ GtkWidget *ShowNewRiwayatDataPage(struct riwayat *new_riwayat, char *namaHolder,
 void on_submit_riwayat_pasien(GtkButton *button, gpointer user_data);
 
 GtkWidget *RiwayatPasienPage_TambahData();
+// -------- END OF TAMBAH DATA PASIEN PAGE --------
+
+
+// -------- UBAH DATA RIWAYAT PASIEN PAGE --------
+void reset_ubahRiwayat_pasien_page();
+
+void on_back_ubahRiwayatPasien_gui(GtkButton *button, gpointer user_data);
+
+void on_submit_ubahRiwayat_pasien_page(GtkButton *button, gpointer user_data);
+
+GtkWidget *ubahRiwayat_form_page(int nomorRiwayat);
+
+void on_ubah_riwayatPasien_gui(GtkButton *button, gpointer user_data);
+
+void on_cari_riwayatPasien_ubahData_gui(GtkButton *button, gpointer user_data);
+
+GtkWidget *RiwayatPasienPage_UbahData();
+// -------- END OF UBAH DATA PASIEN PAGE --------
+
+// -------- HAPUS DATA RIWAYAT PASIEN PAGE --------
+void reset_hapusRiwayat_pasien_page();
+
+void on_back_hapusRiwayatPasien_gui(GtkButton *button, gpointer user_data);
+
+void on_hapus_riwayatPasien_gui(GtkButton *button, gpointer user_data);
+
+void on_cari_riwayatPasien_hapusData_gui(GtkButton *button, gpointer user_data);
+
+GtkWidget *RiwayatPasienPage_HapusData();
+// -------- END OF HAPUS DATA PASIEN PAGE --------
 
 // Base Page
 GtkWidget *RiwayatPasienPage();
