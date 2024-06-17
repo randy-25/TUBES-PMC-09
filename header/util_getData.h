b3dfd1a0b2b6caaed6c24ba14a9aaf2d253fbf88
randy-25 <randyrevaldo64@gmail.com>
2024-06-17 14:17:47 +0700

    Change the reading file to csv

--- header/util_getData.h
+++ header/util_getData.h
@@ -1,24 +1,20 @@
-#ifndef GETDATA_H
-#define GETDATA_H
+#ifndef UTIL_GETDATACSV_H
+#define UTIL_GETDATACSV_H
 
-#include <xlsxio_read.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 #include "dataType.h"
 
-void getDataPasien(struct dataPasien *pasien, char *value, int colIndex);
-void printDataPasien(struct dataPasien pasien);
-void getRiwayatPasien(struct riwayat *riwayatPasien, char *value, int colIndex);
-void getBiaya(struct biaya **biaya);
-void printRiwayatPasien(struct riwayat riwayatPasien);
-int getMonthNumber(const char *monthName);
-int isNumeric(const char *str);
-void convertDaysToDate(int days, int *date, int *month, int *year);
-void parseDateString(const char *dateString, int *date, int *month, int *year);
+int getMonthNumber_CSV(const char *monthName);
+int is_format_CSV(char *Date);
+void printDataPasien_CSV (struct dataPasien DataPasien);
+void printRiwayatPasien_CSV (struct riwayat RiwayatPasien);
+void parseDate_CSV (char *Date, int *day, int *month, int *year);
+void get_DataPasien_CSV (struct dataPasien **Data, int *JumlahDataPasien);
+void get_RiwayatPasien_CSV (struct riwayat **RiwayatPasien, int *JumlahRiwayatPasien);
+void get_BiayaTindakan_CSV (struct biaya **BiayaPerawatan);
 void parseTanggal(char *tanggalLahir_input, int *tanggal, int *bulan, int *tahun);
-void getData(struct dataPasien **pasien, struct riwayat **riwayatPasien, int *jumlahPasien, int *jumlahRiwayatPasien);
 
 #endif
